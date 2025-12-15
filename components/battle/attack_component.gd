0a51a20cb429290008cf857ea113d1dabb87b6ac
Bryton Lacquement <hello@brytonlacquement.com>
2025-12-15 19:09:16 +0100

    fixup! Add the AttackComponent
    This fixes commit 50d3ba95e094cc3f95b813d97031518ba22e9ab8.

--- components/battle/attack_component.gd
+++ components/battle/attack_component.gd
@@ -3,28 +3,25 @@ extends Node
 
 signal defenders_changed(defenders: Array[HealthComponent])
 
-
 @export_group("Required")
 @export var character: Character
 
 @export_group("Parameters")
 
-
 var _defenders: Array[HealthComponent]
-
 
 
 func attack(damage_callable: Callable, amount = -1):
 	var attacked := 0
-		
+
 	for health_component in _defenders:
 		if health_component.is_dead():
 			continue
 
-		var damage:int = damage_callable.call(health_component)
+		var damage: int = damage_callable.call(health_component)
 		health_component.apply_damage(damage)
 		attacked += 1
-				
+
 		# Stop if we've reached the desired amount (unless amount == -1)
 		if amount > 0 and attacked >= amount:
 			break
