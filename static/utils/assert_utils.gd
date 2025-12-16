871b5970a78f5b108435122b5b45339c60730cb6
Bryton Lacquement <hello@brytonlacquement.com>
2025-12-16 13:27:07 +0100

    fixup! Add AssertUtils
    This fixes commit 96667ddc4389af3efb921a801b583f04242dfa41.

--- static/utils/assert_utils.gd
+++ static/utils/assert_utils.gd
@@ -2,5 +2,5 @@ class_name AssertUtils
 extends Object
 
 
-static func _assert_has_signal(obj: Object, signal_name: String):
+static func assert_has_signal(obj: Object, signal_name: String):
 	assert(obj.has_signal(signal_name), "Object %s does not have signal '%s'" % [obj, signal_name])
