b2caa3e988f97509a2a06c78e3b2db6885a2fe6e
Bryton Lacquement <hello@brytonlacquement.com>
2025-12-16 14:07:05 +0100

    AssertUtils: add assertions for method and property

--- static/utils/assert_utils.gd
+++ static/utils/assert_utils.gd
@@ -2,5 +2,17 @@ class_name AssertUtils
 extends Object
 
 
-static func assert_has_signal(obj: Object, signal_name: String):
-	assert(obj.has_signal(signal_name), "Object %s does not have signal '%s'" % [obj, signal_name])
+static func assert_has_method(obj: Object, method_name: String) -> void:
+	assert(obj.has_method(method_name), _error_message(obj, "method", method_name))
+
+
+static func assert_has_property(obj: Object, property_name: String) -> void:
+	assert(obj.has_property(property_name), _error_message(obj, "property", property_name))
+
+
+static func assert_has_signal(obj: Object, signal_name: String) -> void:
+	assert(obj.has_signal(signal_name), _error_message(obj, "signal", signal_name))
+
+
+static func _error_message(obj: Object, type: String, name: String) -> String:
+	return "Object %s does not have %s '%s'" % [obj, type, name]
