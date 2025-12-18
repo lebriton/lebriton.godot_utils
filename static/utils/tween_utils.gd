787c2d051738eac64cfba9dddcd19b189dabddde
Bryton Lacquement <hello@brytonlacquement.com>
2025-12-18 16:42:20 +0100

    TweenUtils: add get_or_create_tween

--- static/utils/tween_utils.gd
+++ static/utils/tween_utils.gd
@@ -2,6 +2,17 @@ class_name TweenUtils
 extends Object
 
 
+static func get_or_create_tween(node: Node, meta_name: StringName = "tween") -> Tween:
+	if node.has_meta(meta_name):
+		var tween := node.get_meta(meta_name)
+		if tween and tween.is_valid():
+			return tween
+
+	var tween := node.create_tween()
+	node.set_meta(meta_name, tween)
+	return tween
+
+
 static func is_tween_running(tween: Tween) -> bool:
 	return tween and tween.is_running()
 
