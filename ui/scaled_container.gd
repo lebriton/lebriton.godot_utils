0933bd1cf73f0553c345b3085d9803780510bc3a
Bryton Lacquement <hello@brytonlacquement.com>
2025-12-24 17:11:45 +0100

    ScaledContainer: introduce scale modes

--- ui/scaled_container.gd
+++ ui/scaled_container.gd
@@ -4,6 +4,9 @@
 class_name ScaledContainer
 extends Container
 
+enum ScaleMode { FIT, CENTER }
+
+@export var scale_mode := ScaleMode.FIT
 @export var content_scale := 1.0:
 	set(v):
 		content_scale = v
@@ -20,15 +23,25 @@ func _get_minimum_size() -> Vector2:
 		min_size.x = max(min_size.x, child.get_combined_minimum_size().x)
 		min_size.y = max(min_size.y, child.get_combined_minimum_size().y)
 
-	return min_size * content_scale
+	if scale_mode == ScaleMode.FIT:
+		min_size *= content_scale
+
+	return min_size
 
 
 func _notification(what: int):
 	match what:
 		NOTIFICATION_SORT_CHILDREN:
 			for child: Control in get_children():
-				child.set_anchors_and_offsets_preset(
-					Control.PRESET_FULL_RECT, Control.PRESET_MODE_MINSIZE
-				)
+				var preset: LayoutPreset
+
+				match scale_mode:
+					ScaleMode.FIT:
+						preset = Control.PRESET_FULL_RECT
+					ScaleMode.CENTER:
+						preset = Control.PRESET_CENTER
+						child.pivot_offset = child.size / 2.0
+
+				child.set_anchors_and_offsets_preset(preset, Control.PRESET_MODE_MINSIZE)
 				child.size /= content_scale
 				child.scale = Vector2(content_scale, content_scale)
