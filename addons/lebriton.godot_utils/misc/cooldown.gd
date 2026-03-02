080c2842990b83bd018b21ab08cb0ef9a43237dd
Bryton Lacquement <hello@brytonlacquement.com>
2026-03-02 01:08:12 +0100

    Cooldown: improve code

--- addons/lebriton.godot_utils/misc/cooldown.gd
+++ addons/lebriton.godot_utils/misc/cooldown.gd
@@ -4,15 +4,15 @@ extends Resource
 @export_group("Required")
 
 @export_group("Parameters")
-@export var duration: float = 1.0
+@export_custom(PROPERTY_HINT_NONE, "suffix:ms") var duration: int = 200
 
 var _last_time: float = -1.0
 
 
-func ready() -> bool:
+func is_ready() -> bool:
 	if _last_time < 0:
 		return true
-	return Time.get_ticks_msec() - _last_time >= duration * 1000
+	return Time.get_ticks_msec() - _last_time >= duration
 
 
 func reset():
