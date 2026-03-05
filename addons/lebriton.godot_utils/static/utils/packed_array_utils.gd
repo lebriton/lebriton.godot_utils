f94c993dfe87da60b056c523d5d1c6eba1fed4d4
Bryton Lacquement <hello@brytonlacquement.com>
2026-03-05 23:27:25 +0100

    Add missing static

--- addons/lebriton.godot_utils/static/utils/packed_array_utils.gd
+++ addons/lebriton.godot_utils/static/utils/packed_array_utils.gd
@@ -8,13 +8,13 @@ extends Object
 # PackedArrays, and they are not always easy to work with directly.
 
 
-func erase(packed_array, value: Variant):
+static func erase(packed_array, value: Variant):
 	var index = packed_array.find(value)
 	if index != -1:
 		packed_array.remove_at(index)
 
 
-func filter(packed_array, method: Callable) -> Array:
+static func filter(packed_array, method: Callable) -> Array:
 	var result_array = []
 
 	for i in range(packed_array.size()):
@@ -24,7 +24,7 @@ func filter(packed_array, method: Callable) -> Array:
 	return result_array
 
 
-func map(packed_array, method: Callable):
+static func map(packed_array, method: Callable):
 	var result_array = packed_array.duplicate()
 
 	for i in range(result_array.size()):
