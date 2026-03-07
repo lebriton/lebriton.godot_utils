9880c339dcb13dd7edf1937bdf9462e2fd11e251
Bryton Lacquement <hello@brytonlacquement.com>
2026-03-07 20:05:22 +0100

    get_animated_sprite_current_texture: fix null sprite_frames

--- addons/lebriton.godot_utils/static/utils/sprite_utils.gd
+++ addons/lebriton.godot_utils/static/utils/sprite_utils.gd
@@ -3,4 +3,7 @@ extends Object
 
 
 static func get_animated_sprite_current_texture(s) -> Texture2D:
+	if not s.sprite_frames:
+		return null
+
 	return s.sprite_frames.get_frame_texture(s.animation, s.frame)
