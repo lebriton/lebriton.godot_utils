1d8e78d324877e96fd091f0bb487021cab88dda7
Bryton Lacquement <hello@brytonlacquement.com>
2026-03-12 11:18:25 +0100

    fixup! project_on_plane: make it more generic
    This fixes commit 45b4bbf50edc332808ace204c2f85deac951e3ed.

--- addons/lebriton.godot_utils/static/utils/camera_utils.gd
+++ addons/lebriton.godot_utils/static/utils/camera_utils.gd
@@ -2,7 +2,7 @@ class_name CameraUtils
 extends Object
 
 
-static func project_on_plane(camera: Camera3D, plane: Plane, position: Vector2) -> Vector3:
+static func project_on_plane(camera: Camera3D, plane: Plane, position: Vector2) -> Variant:
 	var from = camera.project_ray_origin(position)
 	var to = camera.project_ray_normal(position)
 	return plane.intersects_ray(from, to)
