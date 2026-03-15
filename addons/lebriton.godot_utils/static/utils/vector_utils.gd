ff326ed915bf8b6f488343f7e103533b9c8de4d0
Bryton Lacquement <hello@brytonlacquement.com>
2026-03-15 13:22:41 +0100

    VectorUtils: stuff

--- addons/lebriton.godot_utils/static/utils/vector_utils.gd
+++ addons/lebriton.godot_utils/static/utils/vector_utils.gd
@@ -10,17 +10,11 @@ static func vec2_to_2i(vector: Vector2) -> Vector2i:
 	return Vector2i(vector)
 
 
-static func vec2_to_2i_with_remaining(vector: Vector2) -> Array:
-	var new_vector := Vector2i(vector)
-	var remaining := vector - Vector2(new_vector)
-	return [new_vector, remaining]
-
-
 static func vec2_to_3(vector: Vector2, y: float = 0.0) -> Vector3:
 	return Vector3(vector.x, y, vector.y)
 
 
-static func vec2_to_3i(vector: Vector2, y: int = 0) -> Vector3:
+static func vec2_to_3i(vector: Vector2, y: int = 0) -> Vector3i:
 	return Vector3i(vector.x, y, vector.y)
 
 
@@ -32,7 +26,7 @@ static func vec2i_to_3(vector: Vector2i, y: float = 0.0) -> Vector3:
 	return Vector3(vector.x, y, vector.y)
 
 
-static func vec2i_to_3i(vector: Vector2i, y: int = 0) -> Vector3:
+static func vec2i_to_3i(vector: Vector2i, y: int = 0) -> Vector3i:
 	return Vector3i(vector.x, y, vector.y)
 
 
@@ -40,7 +34,7 @@ static func vec3_to_2(vector: Vector3) -> Vector2:
 	return Vector2(vector.x, vector.z)
 
 
-static func vec3_to_2i(vector: Vector3) -> Vector2:
+static func vec3_to_2i(vector: Vector3) -> Vector2i:
 	return Vector2i(vector.x, vector.z)
 
 
@@ -48,5 +42,9 @@ static func vec3i_to_2(vector: Vector3i) -> Vector2:
 	return Vector2(vector.x, vector.z)
 
 
-static func vec3i_to_2i(vector: Vector3i) -> Vector2:
+static func vec3i_to_2i(vector: Vector3i) -> Vector2i:
 	return Vector2i(vector.x, vector.z)
+
+
+static func vec3i_to_3(vector: Vector3i) -> Vector3:
+	return Vector3(vector.x, vector.y, vector.z)
