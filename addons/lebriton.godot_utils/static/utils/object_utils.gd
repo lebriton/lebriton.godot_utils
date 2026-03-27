1558b039fcfac1942b8dd2a68263220334a612df
Bryton Lacquement <hello@brytonlacquement.com>
2026-03-27 22:28:06 +0100

    Add ObjectUtils

---
+++ addons/lebriton.godot_utils/static/utils/object_utils.gd
@@ -0,0 +1,18 @@
+class_name ObjectUtils
+extends Object
+
+
+static func swap_node_value_and_update_signals(
+	object: Object,
+	property: StringName,
+	new_value: Node,
+	signal_handlers: Dictionary,
+	bind_node: bool = false,
+):
+	var previous_value = object.get(property)
+	if previous_value:
+		SignalUtils.disconnect_signals(previous_value, signal_handlers, bind_node)
+	object.set(property, new_value)
+
+	if new_value:
+		SignalUtils.connect_signals(new_value, signal_handlers, bind_node)
