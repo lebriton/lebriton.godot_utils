103e120d746eb284c28b32e1b9812164142c5f84
Bryton Lacquement <hello@brytonlacquement.com>
2026-03-27 11:40:25 +0100

    Vector2Utils: add vector_2_grid_dir

--- addons/lebriton.godot_utils/static/utils/vector2_utils.gd
+++ addons/lebriton.godot_utils/static/utils/vector2_utils.gd
@@ -2,6 +2,17 @@ class_name Vector2Utils
 extends Object
 
 
+static func vector_to_grid_dir(direction: Vector2) -> Vector2:
+	if direction == Vector2.ZERO:
+		return Vector2.ZERO
+
+	var angle = atan2(direction.y, direction.x)
+	var step = PI / 4.0  # 45°
+
+	var snapped = round(angle / step) * step
+	return Vector2(cos(snapped), sin(snapped)).round()
+
+
 static func smooth_to(
 	current: Vector2, target: Vector2, smoothing: bool = true, weight: float = 0.5
 ) -> Vector2:
