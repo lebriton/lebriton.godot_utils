6c3c679f8bafd8ad5b52f7d00219ef1223899bbb
Bryton Lacquement <hello@brytonlacquement.com>
2026-03-29 00:08:00 +0100

    SignalUtils.disconnect_signals: drop bind_node

--- addons/lebriton.godot_utils/static/utils/signal_utils.gd
+++ addons/lebriton.godot_utils/static/utils/signal_utils.gd
@@ -12,11 +12,8 @@ static func connect_signals(node: Node, signal_handlers: Dictionary, bind_node: 
 	)
 
 
-static func disconnect_signals(node: Node, signal_handlers: Dictionary, bind_node: bool = false):
+static func disconnect_signals(node: Node, signal_handlers: Dictionary):
 	DictUtils.for_each(
 		signal_handlers,
-		func(signal_name, signal_handler):
-			if bind_node:
-				signal_handler = signal_handler.bind(node)
-			node.disconnect(signal_name, signal_handler)
+		func(signal_name, signal_handler): node.disconnect(signal_name, signal_handler)
 	)
