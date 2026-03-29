bfd11a7bc74a76ed739b2b2247d6e045f12ff7e8
Bryton Lacquement <hello@brytonlacquement.com>
2026-03-29 01:51:24 +0100

    Some stuff about signals and meta

--- addons/lebriton.godot_utils/static/utils/meta_utils.gd
+++ addons/lebriton.godot_utils/static/utils/meta_utils.gd
@@ -2,6 +2,14 @@ class_name MetaUtils
 extends Object
 
 
+static func get_or_create_meta(obj: Object, name: StringName, default_factory: Callable) -> Variant:
+	if obj.has_meta(name):
+		return obj.get_meta(name)
+	var value = default_factory.call()
+	obj.set_meta(name, value)
+	return value
+
+
 static func set_dict(obj: Object, dict: Dictionary):
 	for meta_name in dict:
 		var meta_value = dict[meta_name]
--- addons/lebriton.godot_utils/static/utils/signal_utils.gd
+++ addons/lebriton.godot_utils/static/utils/signal_utils.gd
@@ -2,14 +2,29 @@ class_name SignalUtils
 extends Object
 
 
-static func connect_signals(node: Node, signal_handlers: Dictionary, bind_node: bool = false):
+static func connect_signals(obj: Object, signal_handlers: Dictionary, bind_obj: bool = false):
 	DictUtils.for_each(
 		signal_handlers,
 		func(signal_name, signal_handler):
-			if bind_node:
-				signal_handler = signal_handler.bind(node)
-			node.connect(signal_name, signal_handler)
+			if bind_obj:
+				signal_handler = signal_handler.bind(obj)
+			obj.connect(signal_name, signal_handler)
 	)
+
+
+static func connect_with_id(id: Variant, obj: Object, signal_name: StringName, callable: Callable):
+	var signal_handlers = MetaUtils.get_or_create_meta(obj, "signal_handlers", func(): return {})
+	signal_handlers[id] = callable
+	obj.connect(signal_name, callable)
+
+
+static func disconnect_by_id(id: Variant, obj: Object, signal_name: StringName):
+	var signal_handlers = MetaUtils.get_or_create_meta(obj, "signal_handlers", func(): return {})
+	if signal_handlers.has(id):
+		var callable = signal_handlers[id]
+		if obj.is_connected(signal_name, callable):
+			obj.disconnect(signal_name, callable)
+		signal_handlers.erase(id)
 
 
 static func disconnect_signals(node: Node, signal_handlers: Dictionary):
