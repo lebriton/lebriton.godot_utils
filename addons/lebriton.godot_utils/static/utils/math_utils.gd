40470ec77bccc6a6e7e1905f4307743688629ab4
Bryton Lacquement <hello@brytonlacquement.com>
2026-04-07 22:22:18 +0200

    MathUtils: add looped_clamp

--- addons/lebriton.godot_utils/static/utils/math_utils.gd
+++ addons/lebriton.godot_utils/static/utils/math_utils.gd
@@ -9,3 +9,12 @@ static func list_to_deltas(numbers: Array) -> Array:
 		deltas.append(numbers[i] - numbers[i - 1])
 
 	return deltas
+
+
+static func looped_clamp(value: int, min_value: int, max_value: int) -> int:
+	var range := max_value - min_value + 1
+
+	if range <= 0:
+		return min_value
+
+	return min_value + ((value - min_value) % range + range) % range
