9ab02bde5f8297b40871f25de48c27c308e90825
Bryton Lacquement <hello@brytonlacquement.com>
2026-04-08 13:23:35 +0200

    PrototypeCloner: add set_clone_count

--- addons/lebriton.godot_utils/misc/prototype_cloner.gd
+++ addons/lebriton.godot_utils/misc/prototype_cloner.gd
@@ -8,13 +8,16 @@ extends Node
 @export_group("Parameters")
 @export var auto_detach_prototype := false
 
-var _copies: Array[Node] = []:
-	get():
-		_copies = _copies.filter(func(c): return is_instance_valid(c))
-		return _copies
+var _copies: Array[Node] = []
+
+
+func _cleanup_copies():
+	_copies = _copies.filter(func(c): return is_instance_valid(c))
 
 
 func clear_copies():
+	_cleanup_copies()
+
 	for copy in _copies:
 		copy.queue_free()
 	_copies.clear()
@@ -32,9 +35,23 @@ func detach_prototype():
 
 
 func get_copies() -> Array[Node]:
+	_cleanup_copies()
+
 	return _copies
 
 
 func _ready():
 	if auto_detach_prototype:
 		NodeUtils.when_ready(prototype, detach_prototype)
+
+
+func set_clone_count(count: int) -> void:
+	_cleanup_copies()
+
+	# Remove extra copies if we have too many
+	while _copies.size() > count:
+		_copies.pop_back().queue_free()
+
+	# Add missing copies if we have too few
+	while _copies.size() < count:
+		clone()
