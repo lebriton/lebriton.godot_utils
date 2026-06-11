97e03e4ebb86bf5a7022b2f44d03683f8570aa68
Bryton Lacquement <hello@brytonlacquement.com>
2026-06-11 13:54:55 +0200

    MathUtils: add remap_{float,int}

--- addons/lebriton.godot_utils/static/utils/math_utils.gd
+++ addons/lebriton.godot_utils/static/utils/math_utils.gd
@@ -18,3 +18,29 @@ static func looped_clamp(value: int, min_value: int, max_value: int) -> int:
 		return min_value
 
 	return min_value + ((value - min_value) % range + range) % range
+
+
+static func remap_float(
+	value: float, from_min: float, from_max: float, to_min: float, to_max: float
+) -> float:
+	if is_equal_approx(from_min, from_max):
+		return to_min
+
+	return to_min + (value - from_min) * (to_max - to_min) / (from_max - from_min)
+
+
+static func remap_int(value: int, from_min: int, from_max: int, to_min: int, to_max: int) -> int:
+	if from_min == from_max:
+		return to_min
+
+	return clampi(
+		(
+			to_min
+			+ (
+				((value - from_min) * (to_max - to_min) + (from_max - from_min) / 2)
+				/ (from_max - from_min)
+			)
+		),
+		mini(to_min, to_max),
+		maxi(to_min, to_max)
+	)
